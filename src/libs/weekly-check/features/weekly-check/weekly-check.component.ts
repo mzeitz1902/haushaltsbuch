a47be1357af2968a61a097fe034831ed8aa895ad
Manu <manuel.zeitz@specificobjects.de>
2026-01-24 20:17:22 +0100

    Add month selection dropdown to weekly check component

--- src/libs/weekly-check/features/weekly-check/weekly-check.component.ts
+++ src/libs/weekly-check/features/weekly-check/weekly-check.component.ts
@@ -1,12 +1,21 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { AppHeaderComponent } from '@haushaltsbuch/shared/ui-components';
 import { WeeklyCheckFacade } from '@haushaltsbuch/weekly-check/domain';
 import { WeekComponent } from './week/week.component';
 import { CdkAccordion } from '@angular/cdk/accordion';
+import { NgSelectComponent } from '@ng-select/ng-select';
+import dayjs from 'dayjs';
+import { form, FormField } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-weekly-check',
-  imports: [AppHeaderComponent, WeekComponent, CdkAccordion],
+  imports: [
+    AppHeaderComponent,
+    WeekComponent,
+    CdkAccordion,
+    NgSelectComponent,
+    FormField,
+  ],
   templateUrl: './weekly-check.component.html',
 })
 export class WeeklyCheckComponent {
@@ -15,7 +24,18 @@ export class WeeklyCheckComponent {
   weeklyChecks = this.facade.weeklyChecks;
   isLoading = this.facade.isLoading;
 
+  formModel = signal<{ month: { label: string; value: string } }>({
+    month: { label: dayjs().format('MM'), value: dayjs().format('YYYY-MM-DD') },
+  });
+
+  form = form(this.formModel);
+
   constructor() {
     this.facade.loadWeeklyChecks();
   }
+
+  readonly months = Array.from({ length: 12 }, (_, i) => {
+    const d = dayjs().month(i);
+    return { label: d.format('MMMM'), value: d.format('YYYY-MM-DD') };
+  });
 }
