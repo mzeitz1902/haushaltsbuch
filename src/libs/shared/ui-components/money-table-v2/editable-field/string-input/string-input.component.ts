1b632c3b471d0e1d0542876ee010ad041b4e544c
Manu <manuel.zeitz@specificobjects.de>
2026-01-29 15:31:46 +0100

    Refactor string input component to use ngModel for two-way data binding

--- src/libs/shared/ui-components/money-table-v2/editable-field/string-input/string-input.component.ts
+++ src/libs/shared/ui-components/money-table-v2/editable-field/string-input/string-input.component.ts
@@ -1,16 +1,16 @@
 import { Component, model, output } from '@angular/core';
 import { FormValueControl } from '@angular/forms/signals';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-string-input',
-  imports: [],
+  imports: [FormsModule],
   template: `
     <input
       #inputRef
       type="text"
       class="mb-1 w-24 rounded border-1 border-blue-400 bg-gray-600 p-1 outline-none"
-      [value]="value()"
-      (input)="value.set($event.target.value)"
+      [(ngModel)]="value"
       (blur)="blurred.emit()"
       (keyup.enter)="enterPress.emit()"
     />
